fca6ef2fc2c7c96bcb28581c065f4fb03aec737e
tejaswasnik <77103308+tejaswasnik@users.noreply.github.com>
2021-02-10 15:21:50 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -5,12 +5,12 @@ from tobrot.sample_config import Config
 #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
-    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
+    TG_BOT_TOKEN= "1634255419:AAGqptgSfVushtPCVcq5mTmmu4uHRQI-qsI"
+    APP_ID = 3955129
+    API_HASH = "aa5fa0ab9778c511b12569448a017780"
+    OWNER_ID = 1326980082
+    AUTH_CHANNEL = [-514414375]
+    DESTINATION_FOLDER = "Bingeit MirrorBot #1" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     RCLONE_CONFIG = """
